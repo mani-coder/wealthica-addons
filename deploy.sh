f02e1ee1b325430384663ffc8366d017dfe15e3d
Elayamani Krishnamoorthi <k.elayamani@gmail.com>
2021-06-12 21:05:04 -0700

    abort even if one command fails

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+set -e
 
 echo "Deleting build files ..."
 rm -rf public/mani-coder
 
 echo "Building Addon ..."
 cd addon/pnl
+npm install
 npm run build
 
 cd ../../
 
 echo "Building Widget ..."
 cd widgets/pnl
+npm install
 npm run build
 
 cd ../../
