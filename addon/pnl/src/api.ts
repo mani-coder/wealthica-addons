aa444299fa0fa9a8d3ebb64ae50a8dab622fc41c
Eric <eric.cef@gmail.com>
2021-07-07 14:52:19 -0500

    Fix Deposits amount for transfers
    Following up on the issue https://github.com/mani-coder/wealthica-addons/issues/4 I noticed that the code change made did not fix it.
    I was checking the code, and I see that there is a check if a transaction has a symbol. In my case, the transaction has a symbol, because I am transferring a position from one account to another.
    
    For example:
    Imagine that at this moment, Short therm Account has total deposits amount of 1000 and Long therm Account has total deposits amount of 2000.
    Then, we transfer some positions from Short therm to Long therm, generating these two transactions:
    
    Account: Short therm
    Type: Transfer
    Symbol: GLDM
    Amount: -100
    Quantity: -2.5
    
    Account: Long therm
    Type: Transfer
    Symbol: GLDM
    Amount: 100
    Quantity: 2.5
    
    The expected final result should be:
    Short therm Account has total deposits amount of 900 and Long therm Account has total deposits amount of 2100.
    
    Does this change make sense? We can discuss further to clarify.

--- addon/pnl/src/api.ts
+++ addon/pnl/src/api.ts
@@ -108,10 +108,7 @@ export const parseTransactionsResponse = (response: any, currencyCache: any, acc
       if (['deposit'].includes(type)) {
         portfolioData.deposit += amount;
       } else if (type === 'transfer') {
-        // Ignore security transfers...
-        if (!transaction.symbol) {
           portfolioData.deposit += amount;
-        }
       } else if (['fee', 'interest', 'tax'].includes(type)) {
         portfolioData.interest += Math.abs(amount);
       } else if (['income', 'dividend', 'distribution'].includes(type)) {
