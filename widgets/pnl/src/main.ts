eb41138f695bc9c056ee03b27eb6bf88b843a552
Elayamani K <k.elayamani@gmail.com>
2026-01-02 12:36:12 -0800

    load the dayjs plugins

--- widgets/pnl/src/main.ts
+++ widgets/pnl/src/main.ts
@@ -1,5 +1,12 @@
 import { mount } from 'svelte';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import isoWeek from 'dayjs/plugin/isoWeek';
 import App from './App.svelte';
+
+// Configure dayjs plugins globally
+dayjs.extend(utc);
+dayjs.extend(isoWeek);
 
 const app = mount(App, { target: document.body });
 
