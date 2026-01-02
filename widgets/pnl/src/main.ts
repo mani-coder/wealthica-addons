388ce973cb3150825a696986a3116248e7e2d83b
Elayamani K <k.elayamani@gmail.com>
2026-01-02 12:36:42 -0800

    lint fix

--- widgets/pnl/src/main.ts
+++ widgets/pnl/src/main.ts
@@ -1,7 +1,7 @@
-import { mount } from 'svelte';
 import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
 import isoWeek from 'dayjs/plugin/isoWeek';
+import utc from 'dayjs/plugin/utc';
+import { mount } from 'svelte';
 import App from './App.svelte';
 
 // Configure dayjs plugins globally
